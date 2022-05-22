0ae797e35624284318f1d241b45234f4a1024f6a
Jack Scrivener <10jack222@gmail.com>
2022-05-22 16:43:34 +1200

    Added v3, coppyed v1 to v2 and v2 to the new v3 and removed the comments from v1

--- 4_Cash_or_Card v1/4_Cash_or_Card v1.cpp
+++ 4_Cash_or_Card v1/4_Cash_or_Card v1.cpp
@@ -1,19 +1,13 @@
-// 4_Cash_or_Card.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 #include <algorithm>
 using namespace::std;
 
-//A function that takes a question and returns a bool of weather they selcted true or false 
 bool cash_or_card(string question) {
     cout << question << "\n  :";
     string user_input;
-    // loops the function till a valid input of eyther card or cash is inputed 
     while (true) {
         getline(cin, user_input);
-        //makes the users input into all lower case 
         for_each(user_input.begin(), user_input.end(), [](char& c) {
             c = ::tolower(c);
             });
@@ -24,7 +18,6 @@ bool cash_or_card(string question) {
             return true;
         }
         else {
-            // The error message printed if the user's input is not cash or card
             cout << "plese enter your payment method eyther cash or card\n  :";
         }
     }
