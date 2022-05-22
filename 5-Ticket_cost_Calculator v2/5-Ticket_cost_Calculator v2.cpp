11d5d49dad2a20be28889d22840cb0e93e9f2753
Jack Scrivener <10jack222@gmail.com>
2022-05-22 18:45:52 +1200

    Revert "Revert "Updated main loop to print ticket profit as well""
    This reverts commit 5fa038369fdcc345b366fa423aa6c3a34831d8d2.

--- 5-Ticket_cost_Calculator v2/5-Ticket_cost_Calculator v2.cpp
+++ 5-Ticket_cost_Calculator v2/5-Ticket_cost_Calculator v2.cpp
@@ -10,7 +10,7 @@ float ticket_prices[3] = { 7.5,10.5,6.5 };
 float ticket_profits[3] = { 2.5,5.5,1.5 };
 
 //This function takes the age of the customet and then checks what pricing range they fit into. It then returns the profit and price of their ticket
-void ticket_cost_calculator(int age, float& ticket_price, float& ticket_profit) {
+void ticket_cost_calculator(int age, float &ticket_price, float &ticket_profit) {
     //this loop runs for the number of items in the ticket_prices list
     for (int f = 0; f < sizeof(ticket_prices) / sizeof(ticket_prices[0]); f += 1) {
         if (age >= ticket_cost_ranges[f][0] and age <= ticket_cost_ranges[f][1]) {
@@ -28,8 +28,11 @@ int main()
         int age;
         cout << "enter your age\n  :";
         cin >> age;
-        float ticket_price = ticket_cost_calculator(age);
-        cout << "ticket_price : " << ticket_price << endl;
+        float ticket_price;
+        float ticket_profit;
+        ticket_cost_calculator(age,ticket_price,ticket_profit);
+        cout << "ticket price : " << ticket_price << endl;
+        //cout << "ticket profit :" << ticket_profit << endl;
     }
 
 }
