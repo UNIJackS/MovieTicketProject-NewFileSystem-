e868a746d4e99d37dfb329fc030a7d1f316d2e33
Jack Scrivener <10jack222@gmail.com>
2022-05-22 18:43:13 +1200

    Added v3, coppyed v1 to v2 and v2 to the new v3 and removed the comments from v1

--- 4_Cash_or_Card v2/4_Cash_or_Card v2.cpp
+++ 4_Cash_or_Card v2/4_Cash_or_Card v2.cpp
@@ -7,25 +7,25 @@
 using namespace::std;
 
 //A function that takes a question and returns a bool of weather they selcted true or false 
-bool yes_or_no(string question, string value1, string value2, string error_message) {
+bool cash_or_card(string question) {
     cout << question << "\n  :";
     string user_input;
-    // loops the function till a valid input of either value 1 or 2
+    // loops the function till a valid input of eyther card or cash is inputed 
     while (true) {
         getline(cin, user_input);
         //makes the users input into all lower case 
         for_each(user_input.begin(), user_input.end(), [](char& c) {
             c = ::tolower(c);
             });
-        if (user_input == value1) {
+        if (user_input == "cash") {
             return false;
         }
-        else if (user_input == value2) {
+        else if (user_input == "card") {
             return true;
         }
         else {
-            // The error message printed if the user's input is not value 1 or 2
-            cout << error_message;
+            // The error message printed if the user's input is not cash or card
+            cout << "plese enter your payment method eyther cash or card\n  :";
         }
     }
 }
@@ -34,7 +34,7 @@ bool yes_or_no(string question, string value1, string value2, string error_messa
 int main()
 {
     while (true) {
-        bool card = yes_or_no("Which payment method would you like to use; cash or card ?");
+        bool card = cash_or_card("Which payment method would you like to use; cash or card ?");
         cout << "card = " << card << endl << endl;
     }
 
